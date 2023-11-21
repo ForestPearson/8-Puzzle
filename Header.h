9341b4f45e78abf6f992afa52b2404002334f407
Forest Pearson <flpearson.fp@gmail.com>
2023-11-21 10:50:49 -0800

    cmd update

--- Header.h
+++ Header.h
@@ -14,27 +14,29 @@ Desc:   A program that solves the 8-Puzzle by using best-first-search and a* wit
 
 using namespace std;
 
-struct State {
-	State() {
+struct State
+{
+	State()
+	{
 		this->fvalue = 0;
 		this->level = 0;
 		this->parent = NULL;
 		this->x = NULL;
 		this->y = NULL;
 	}
-	int board[3][3] = { {} };
+	int board[3][3] = {{}};
 	int level;
 	int fvalue;
-	State* parent;
+	State *parent;
 	int y;
 	int x;
 };
 State greedy(State, State, int heuristic);
-State aTiles(State, State); 
+State aTiles(State, State);
 State aStar(State, State, int heuristic);
 State aConflict(State, State);
-bool compare(State* a, State* b);
-void reconstruct(State* current);
+bool compare(State *a, State *b);
+void reconstruct(State *current);
 void print(State);
 bool isEqual(State, State);
 int misplaced(State, State);
